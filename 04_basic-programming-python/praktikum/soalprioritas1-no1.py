db05311cf8aeffe156af7d1efb899cdfa357223e
eristakirani <eristakirani@gmail.com>
2024-02-28 17:57:48 +0700

    soalprioritas1-no2

--- 04_basic-programming-python/praktikum/soalprioritas1-no1.py
+++ 04_basic-programming-python/praktikum/soalprioritas1-no1.py
@@ -1,7 +1,6 @@
 panjang = 2
 lebar = 4
 luas = panjang * lebar 
-print(luas)
 if luas%2==0:
     print("even rectangle")
 else:
---
+++ 04_basic-programming-python/praktikum/soalprioritas1-no2.py
@@ -0,0 +1,4 @@
+phi = 3.14 
+r = 5 
+volume = 4/3*phi*r*r*r 
+print (volume)
